9b846ccdc1668954baff593119a8e020541ac73f
terry.tao <terry.tao@vechain.com>
2018-06-22 17:04:49 +0800

    添加注释

--- src/erc20/web3server.ts
+++ src/erc20/web3server.ts
@@ -10,6 +10,7 @@ export class Web3Server {
         this.web3 = new Web3();
         thorify(this.web3, host);
     }
+    //添加私钥
     addPrivateKey(privKey: string) {
         this.web3.eth.accounts.wallet.add('0x' + privKey);
         let user = '0x' + ethUtil.privateToAddress(new Buffer(privKey, 'hex')).toString('hex');
@@ -19,19 +20,23 @@ export class Web3Server {
         this.users.push(user);
         return user;
     }
+    //获取block信息
     async getBlock(revision:string|number){
         return this.web3.eth.getBlock(revision);
     }
+    //获取合约cde
     async getCode(addr: string) {
         return this.web3.eth.getCode(addr);
     }
+    //获取balance
     async getBalance(addr: string) {
         return this.web3.eth.getBalance(addr);
     }
+    //获取energy
     async getEnergy(addr: string) {
         return this.web3.eth.getEnergy(addr);
     }
-
+    //实例contract
     Contract(abi, address?:string){
         if(!address) return new this.web3.eth.Contract(abi);
         else return new this.web3.eth.Contract(abi, address);
